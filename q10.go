5473ea143c79eb8243885368c41d61654fe82125
dorukcan <dckisin@gmail.com>
2018-08-23 16:06:13 +0300

    question 10 initialized

---
+++ q10.go
@@ -0,0 +1,18 @@
+/*
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+*/
+
+package main
+
+import "fmt"
+
+func solveQ10(target int) int {
+	return 1
+}
+
+func main() {
+	fmt.Println(solveQ10(10))
+	fmt.Println(solveQ10(2000000))
+}
