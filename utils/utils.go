1c770c90af03a7cdab837b9328bc846702f3a207
dorukcan <dckisin@gmail.com>
2018-08-24 21:52:35 +0300

    q17

--- utils/utils.go
+++ utils/utils.go
@@ -253,3 +253,77 @@ func PowStringsAsNumbers(first string, second int) string {
     return result
 }
 
+func NumberToWrittenForm(num int) string {
+    translateMap := map[int]string{
+        1:    "one",
+        2:    "two",
+        3:    "three",
+        4:    "four",
+        5:    "five",
+        6:    "six",
+        7:    "seven",
+        8:    "eight",
+        9:    "nine",
+        10:   "ten",
+        11:   "eleven",
+        12:   "twelve",
+        13:   "thirteen",
+        14:   "fourteen",
+        15:   "fifteen",
+        16:   "sixteen",
+        17:   "seventeen",
+        18:   "eighteen",
+        19:   "nineteen",
+        20:   "twenty",
+        30:   "thirty",
+        40:   "forty",
+        50:   "fifty",
+        60:   "sixty",
+        70:   "seventy",
+        80:   "eighty",
+        90:   "ninety",
+        100:  "hundred",
+        1000: "thousand",
+    }
+
+    num_ := strconv.Itoa(num)
+
+    result := ""
+
+    for index := range num_ {
+        digit := GetDigitFromString(num_, index)
+        temp := int(math.Pow(float64(10), float64(len(num_)-index-1)))
+        val := digit * temp
+
+        if val >= 100 {
+            result += translateMap[digit] + " " + translateMap[temp]
+        } else if val >= 20 {
+            if len(result) > 0 && digit != 0 {
+                result += " and "
+            }
+            result += translateMap[val]
+        } else if val >= 10 {
+            if len(result) > 0 && digit != 0 {
+                result += " and "
+            }
+
+            val := val + GetDigitFromString(num_, index+1)
+            result += translateMap[val]
+            break
+        } else {
+            if digit != 0 && len(num_) > 1 && len(result) > 0 {
+                prev := GetDigitFromString(num_, index-1)
+                if prev == 0 {
+                    result += " and "
+                } else {
+                    result += "-"
+                }
+            }
+
+            result += translateMap[digit]
+        }
+    }
+
+    return result
+}
+
